f4965ef1cf774811a079e31f074944d73ccab640
Unalome81 <108210469+Unalome81@users.noreply.github.com>
2023-11-07 01:09:25 +0530

    Update memsyscall.c
    Completing system calls

--- memsyscall.c
+++ memsyscall.c
@@ -1,6 +1,27 @@
 #include "memsyscall.h"
 // Function to use system call to allocate memory pages
-void *page_allocater(int pages){}
+#define FLAGS (MAP_ANONYMOUS | MAP_PRIVATE)
+void *page_allocater(int pages)
+{
+    size_t size = PAGE_SIZE * pages; // Size of the memory region in bytes 
+
+    // Allocate anonymous memory using mmap
+    void *mem = (void *)mmap(NULL, size, PROT_READ | PROT_WRITE, FLAGS, -1, 0);
+    printf("Memory mapped using mmap.\n");
+    if (mem == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+    return mem;
+}
 
 // Function to use system call to free memory pages
-void page_free(void *addr, int pages){}
+void page_free(void *addr, int pages)
+{
+    size_t size = PAGE_SIZE * pages;
+    if (munmap(addr, size) == -1) {
+        perror("munmap");
+        exit(EXIT_FAILURE);
+    }
+    printf("Memory released using munmap.\n");
+}
